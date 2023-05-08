bdd580f7abfca22ca4c0e2c6dc4fe2d980334ff4
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-05-08 21:22:51 +0300

    required

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -3,26 +3,52 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryUl = document.querySelector('.gallery');
 
-const imgCard = createImgCard(galleryItems);
+// const imgCard = createImgCard(galleryItems);
 
-galleryUl.insertAdjacentHTML('beforeend', imgCard);
-galleryUl.addEventListener('click', onImgCardClick);
+// galleryUl.insertAdjacentHTML('beforeend', imgCard);
+// galleryUl.addEventListener('click', onImgCardClick);
 
-function createImgCard(galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-        return `
-        <li class="gallery__item">
-            <a class="gallery__link" href='${original}'>
-            <img class="gallery__image" src='${preview}' alt='${description}' />
-            </a>
-        </li>`
-    }).join("");
-};
+// function createImgCard(galleryItems) {
+//     return galleryItems.map(({preview, original, description}) => {
+//         return `
+//         <li class="gallery__item">
+//             <a class="gallery__link" href='${original}'>
+//             <img class="gallery__image" src='${preview}' alt='${description}' />
+//             </a>
+//         </li>`
+//     }).join("");
+// };
 
-function onImgCardClick(evt) {
-    evt.preventDefault();
-    if(!evt.target.classList.contains('gallery__image')) {
-        return;
-    }
-};
-const lightbox = new SimpleLightbox('.gallery__link', { captionsData: "alt", captionDelay: "250" });
+// function onImgCardClick(evt) {
+//     evt.preventDefault();
+//     if(!evt.target.classList.contains('gallery__image')) {
+//         return;
+//     }
+// };
+// const lightbox = new SimpleLightbox('.gallery__link', { captionsData: "alt", captionDelay: "250" });
+
+
+function gallery(arr) {
+    return arr.map((items) => markup(items));
+  };
+  function markup(items) {
+    const {preview, original, description} = items;
+    return `<li class="gallery__item">
+    <a class='gallery__link' href='${original}'>
+      <img
+        class='gallery__image'
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'
+      />
+    </a>
+  </li>`;
+  };
+  
+  function renderList() {
+    gallery(galleryItems);
+    galleryUl.innerHTML = gallery(galleryItems).join("");
+  };
+  renderList();
+  
+  new SimpleLightbox(".gallery a", {captionDelay: 250, overlayOpacity: 1, captionsData: "alt"});
